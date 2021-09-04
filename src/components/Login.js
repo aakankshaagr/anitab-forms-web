b42c10a8d6c764cebf70b3c9906f008db466c9ce
Aakanksha Agrawal <aakankshaagr2018@gmail.com>
2021-09-04 13:36:01 +0530

    added preloader near login

--- src/components/Login.js
+++ src/components/Login.js
@@ -13,9 +13,10 @@ import {
 } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import './../styles/Login.css';
+import './../styles/preloader.css';
 import orgLogo from '../assets/org-logo.jpg';
 import { register } from '../urls';
-
+import './../styles/preloader.css';
 class Login extends Component {
   constructor(props) {
     super(props);
@@ -39,6 +40,7 @@ class Login extends Component {
 
   submitLogin = () => {
     let err = false;
+    document.querySelector('.btn-loading').style.visibilty = 'visible';
     if (this.state.username === '') {
       this.setState({
         usernameerror: true,
@@ -94,7 +96,9 @@ class Login extends Component {
   };
 
   onChange = (e) => {
-    this.setState({ [e.target.name]: e.target.value });
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
   };
 
   UNSAFE_componentWillMount() {
@@ -111,15 +115,17 @@ class Login extends Component {
 
   render() {
     const { showPassword, error, submitted } = this.state;
+
     return (
       <>
         <Grid>
+          {' '}
           <Grid.Row>
             <Grid.Column width={5}>
               <div className="login">
                 <span>
-                  <b>Login</b>
-                </span>
+                  <b> Login </b>{' '}
+                </span>{' '}
                 <Divider />
                 <Form>
                   <Form.Input
@@ -150,37 +156,38 @@ class Login extends Component {
                     fluid
                     primary
                     type="button"
+                    className="btn-loading"
                     onClick={this.submitLogin}
                     disabled={!this.state.username || !this.state.password}
                   >
-                    LOG IN
-                  </Button>
-                </Form>
-
-                {/* form validation */}
+                    <span className="btn-text">LOG IN</span>{' '}
+                  </Button>{' '}
+                </Form>{' '}
+                {/* form validation */}{' '}
                 {this.state.usernameerror ? (
                   <Message error content="Username cannot be empty!" />
-                ) : null}
+                ) : null}{' '}
                 {this.state.passworderror ? (
                   <Message error content="Password cannot be empty!" />
-                ) : null}
+                ) : null}{' '}
                 {error && submitted ? (
                   <Message error content={this.props.loginerror.detail} />
-                ) : null}
+                ) : null}{' '}
                 <Divider />
                 <span>
-                  Don't have an account?{' '}
-                  <Link to={register()}>Register here.</Link>
-                </span>
-              </div>
-            </Grid.Column>
+                  Dont have an account ?{' '}
+                  <Link to={register()}> Register here. </Link>{' '}
+                </span>{' '}
+              </div>{' '}
+            </Grid.Column>{' '}
             <Grid.Column width={10}>
               <div className="logo">
-                <Image src={orgLogo} />
-              </div>
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
+                <Image src={orgLogo} />{' '}
+              </div>{' '}
+            </Grid.Column>{' '}
+          </Grid.Row>{' '}
+          ){' '}
+        </Grid>{' '}
       </>
     );
   }
